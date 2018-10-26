d698b7885279f7506858de5b47f9835e3c8fbac2
Victor kakama <vkakama@gmail.com>
2018-10-26 13:53:37 +0300

    sample object will be used for testing object lists in a single selection

---
+++ app/src/main/java/me/riddhimanadib/fastformbuilder/model/Student.java
@@ -0,0 +1,32 @@
+package me.riddhimanadib.fastformbuilder.model;
+
+public class Student {
+    private String uuid;
+    private String name;
+
+    public Student(String uuid, String name) {
+        this.uuid = uuid;
+        this.name = name;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}
